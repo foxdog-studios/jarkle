3e254d3bd57dc0a045c05e57d3f82872532662e6
Peter Sutton <foxxy@foxdogstudios.com>
2013-10-27 12:55:54 +0000

    Updated scripts
    - Added example settings
    - Removed autopublish
    - Updated meteor

---
+++ scripts/mrt.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -o errexit
+set -o nounset
+
+repo=$(realpath "$(dirname "$(realpath -- "${BASH_SOURCE[0]}")")/..")
+cd "$repo/jarkle"
+
+if [[ ! -v METEOR_SETINGS ]]; then
+    METEOR_SETINGS=$repo/conf/site/development-settings.json
+fi
+
+mrt --settings "$METEOR_SETINGS" $@
+
---
+++ scripts/setup/arch-tasks.sh
@@ -0,0 +1,77 @@
+# =============================================================================
+# = Configuration                                                             =
+# =============================================================================
+
+repo=$(realpath "$(dirname "$(realpath -- "${BASH_SOURCE[0]}")")/../..")
+
+aur_packages=( )
+
+env=${repo}/env
+
+node_global_packages=(
+    meteorite
+)
+
+python_packages=(
+    git+https://github.com/foxdog-studios/conf.git@v2.0.2
+)
+
+python_version=2.7
+
+system_packages=(
+    expect
+    git
+    nodejs
+    python2-virtualenv
+    yaourt
+)
+
+
+# =============================================================================
+# = Tasks                                                                     =
+# =============================================================================
+
+create_ve() {
+    "virtualenv-${python_version}" "${env}"
+}
+
+install_global_node_packages() {
+    sudo npm install --global "${node_global_packages[@]}"
+}
+
+install_python_packages() {
+    _ve _install_python_packages
+}
+
+_install_python_packages() {
+    local package
+    for package in "${python_packages[@]}"; do
+        pip install "$package"
+    done
+}
+
+install_system_packages() {
+    sudo pacman --needed --noconfirm --refresh --sync "${system_packages[@]}"
+}
+
+
+# =============================================================================
+# = Helpers                                                                   =
+# =============================================================================
+
+_allow_unset() {
+    local restore=$(set +o | grep nounset)
+    set +o nounset
+    "${@}"
+    local exit_status=${?}
+    # Do no quote, expansion is desired
+    ${restore}
+    return "${exit_status}"
+}
+
+_ve() {
+    _allow_unset source "${env}/bin/activate"
+    "${@}"
+    _allow_unset deactivate
+}
+
