113bd99ea91a4cde1a43b7b60122a243e9bddcac
Peter Sutton <foxxy@foxdogstudios.com>
2014-01-16 21:54:22 +0000

    Updated packages & workaround for device-detection

--- scripts/mrt.sh
+++ scripts/mrt.sh
@@ -1,11 +1,11 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 set -o errexit
 set -o nounset
 
 repo=$(realpath "$(dirname "$(realpath -- "${BASH_SOURCE[0]}")")/..")
 
-$repo/scripts/bower-install.sh
+"$repo/scripts/bower-install.sh" > /dev/null
 
 cd "$repo/jarkle"
 
@@ -13,5 +13,11 @@ if [[ ! -v METEOR_SETINGS ]]; then
     METEOR_SETINGS=$repo/conf/site/development-settings.json
 fi
 
-mrt --settings "$METEOR_SETINGS" $@
+if [[ $# -eq 0 ]]; then
+    args=( --settings "$METEOR_SETINGS" )
+else
+    args=( "$@" )
+fi
+
+exec mrt "${args[@]}"
 
