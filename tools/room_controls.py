3f31d00d86cd9635e2f7deee23232103fcfbc0c4
Peter Sutton <foxxy@foxdogstudios.com>
2014-08-17 15:20:05 +0100

    Update keyboard layout

--- tools/room_controls.py
+++ tools/room_controls.py
@@ -1,14 +1,60 @@
 # -*- coding: utf-8 -*-
+'''
+Layout:
+
+    41 | F     | Disable
+    42 | F#/Gb | Disable
+    43 | G     | Disable
+    44 | G#/Ab | Disable
+    45 | A     | Disable
+    46 | A#/Bb | Disable
+    47 | B     | Disable
+    48 | C     | Disable
+    49 | C#/Db | Disable
+    50 | D     | Disable
+    51 | D#/Eb | Disable
+    52 | E     | Disable
+    53 | F     |
+    54 | F#/Gb |
+    55 | G     |
+    56 | G#/Ab |
+    57 | A     | Show message
+    58 | A#/Bb |
+    59 | B     | Hide message
+    60 | C     |
+    61 | C#/Db |
+    62 | D     |
+    63 | D#/Eb |
+    64 | E     | Single player
+    65 | F     |
+    66 | F#/Gb |
+    67 | G     |
+    68 | G#/Ab |
+    69 | A     |
+    70 | A#/Bb |
+    71 | B     |
+    72 | C     |
+    73 | C#/Db |
+    74 | D     | Everyone
+    75 | D#/Eb |
+    76 | E     |
+    77 | F     |
+
+'''
 
 from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
 
 from argparse import ArgumentParser
+import logging
 
 import ddp
 
 import midimule
+
+
+logger = logging.getLogger(__name__)
 
 
 def get_listener(args):
@@ -25,11 +71,11 @@ class RoomControls(midimule.MidiPortListener):
         self._roomId = roomId
 
         handlers = {
-            (41, 42, 43): self._show_message,
-            (48, 49, 50): self._hide_message,
-            (55, 46, 57): self._enable_single_player,
-            (62, 63, 64): self._enable_all_players,
-            (69, 70, 71, 72, 73, 74, 75, 76, 77): self._disable_all_players,
+            tuple(xrange(41, 52 + 1)): self._disable_all_players,
+            (57,): self._show_message,
+            (59,): self._hide_message,
+            (64,): self._enable_single_player,
+            (74,): self._enable_all_players,
         }
 
         self._handlers = {}
@@ -80,5 +126,5 @@ class RoomControls(midimule.MidiPortListener):
         self._call_methods('disableAllPlayers')
 
     def _call_methods(self, name):
-        self._client.call(name, self._roomId)
+        future = self._client.call(name, self._roomId)
 
